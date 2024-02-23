377c94cc354258b1970666b9342f122c4eb449c0
Sachin Babu <sachinbabu.dev@gmail.com>
2024-02-23 06:58:45 +0530

    First commit

---
+++ src/pages/post.tsx
@@ -0,0 +1,25 @@
+import { client } from '@/sanity/lib/client';
+import React, { useEffect, useState } from 'react'
+
+export async function fetchPosts() {
+    const query = '*[_type == "post"]';
+    const posts = await client.fetch(query);
+    console.log('>>>posts23', posts)
+    return posts;
+  }
+
+  
+const PostMain = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+      fetchPosts().then(setPosts);
+    }, []);
+  
+    console.log('>>>posts', posts)
+  return (
+    <div>PostMain</div>
+  )
+}
+
+export default PostMain
