f8bcb0862c33ba99eac1ba4766673f6ca4ace7d0
Sachin Babu <sachinbabu.dev@gmail.com>
2024-02-24 20:57:04 +0530

    Shadcn ui added

---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ModeToggle } from './darkModeToggle'
+
+const Header = () => {
+  return (
+    <header>
+        this is header
+        <ModeToggle/>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,25 +1,27 @@
-import { client } from '@/sanity/lib/client';
-import React, { useEffect, useState } from 'react'
+import Header from "@/components/header";
+import { client } from "@/sanity/lib/client";
+import React, { useEffect, useState } from "react";
 
 export async function fetchPosts() {
-    const query = '*[_type == "post"]';
-    const posts = await client.fetch(query);
-    console.log('>>>posts23', posts)
-    return posts;
-  }
-
-  
-const PostMain = () => {
-    const [posts, setPosts] = useState([]);
-
-    useEffect(() => {
-      fetchPosts().then(setPosts);
-    }, []);
-  
-    console.log('>>>posts', posts)
-  return (
-    <div>PostMain</div>
-  )
+  const query = '*[_type == "post"]';
+  const posts = await client.fetch(query);
+  console.log(">>>posts23", posts);
+  return posts;
 }
 
-export default PostMain
+const PostMain = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts().then(setPosts);
+  }, []);
+
+    console.log(">>>posts", posts);
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default PostMain;
