7a8e2466842d3bd7b5b41bac12509efd20338586
Sachin Babu <sachinbabu.dev@gmail.com>
2024-02-25 00:47:18 +0530

    build fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/themeProvider";
-
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
 
 export const metadata: Metadata = {
   title: "Starkfolio- Blogs",
